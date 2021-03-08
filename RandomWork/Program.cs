f5c6332792843890d150dc07f403778faa5af02c
David Mills <david.mills2@outlook.com>
2021-03-08 09:02:03 -0800

    Add project files.

---
+++ RandomWork/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RandomWork
+{
+    class Program
+    {
+         static Random r = new Random(123);
+
+        
+        public static int Results()
+        {
+            
+            return r.Next(1,101);
+        }
+        static void Main(string[] args)
+        {
+            int number = 10;
+            int sum = 0;
+            for (int i = 0; i < number; i++)
+            {
+                sum += Results();
+                //Console.WriteLine(Results());
+            }
+            Console.WriteLine(sum);
+            //Console.WriteLine(Results());
+        }
+    }
+}
